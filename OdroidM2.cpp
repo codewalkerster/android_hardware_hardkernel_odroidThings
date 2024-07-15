13d228353f7721e4a678a85e3950544b459d0600
xiane <sangch.go@gmail.com>
2024-07-15 11:14:52 +0900

    ODROID: things: hw: Add more I2C and UART for M2.
    - I2C-3, UART-3, UART-4, UART-5.
    
    Signed-off-by: xiane <sangch.go@gmail.com>
    Change-Id: Ia144333fbefb9637a2f5838c95b7eb5dabca63c6

--- OdroidM2.cpp
+++ OdroidM2.cpp
@@ -10,15 +10,15 @@ OdroidM2::OdroidM2() {
         {"GND", -1, PIN_GND}, {"10", 16, PIN_GPIO|PIN_UART},
         {"11", 0, PIN_GPIO|PIN_UART}, {"12", 1, PIN_GPIO|PIN_PWM},
         {"13", 2, PIN_GPIO|PIN_UART}, {"GND", -1, PIN_GND},
-        {"15", 3, PIN_GPIO|PIN_PWM}, {"16", 4, PIN_GPIO},
-        {"3.3V", -1, PIN_PWR}, {"18", 5, PIN_GPIO},
+        {"15", 3, PIN_GPIO|PIN_PWM}, {"16", 4, PIN_GPIO|PIN_UART},
+        {"3.3V", -1, PIN_PWR}, {"18", 5, PIN_GPIO|PIN_UART},
         {"19", 12, PIN_GPIO|PIN_SPI}, {"GND", -1, PIN_GND},
-        {"21", 13, PIN_GPIO|PIN_SPI}, {"22", 6, PIN_GPIO},
-        {"23", 14, PIN_GPIO|PIN_SPI}, {"24", 10, PIN_GPIO|PIN_SPI},
+        {"21", 13, PIN_GPIO|PIN_SPI}, {"22", 6, PIN_GPIO|PIN_I2C},
+        {"23", 14, PIN_GPIO|PIN_SPI}, {"24", 10, PIN_GPIO|PIN_SPI|PIN_I2C},
         {"25", -1, PIN_GND}, {"26", 11, PIN_GPIO},
-        {"27", 30, PIN_GPIO|PIN_I2C}, {"28", 31, PIN_GPIO|PIN_I2C},
-        {"29", 21, PIN_GPIO}, {"GND", -1, PIN_GND},
-        {"31", 22, PIN_GPIO}, {"32", 26, PIN_GPIO},
+        {"27", 30, PIN_GPIO|PIN_I2C|PIN_UART}, {"28", 31, PIN_GPIO|PIN_I2C|PIN_UART},
+        {"29", 21, PIN_GPIO|PIN_UART}, {"GND", -1, PIN_GND},
+        {"31", 22, PIN_GPIO|PIN_UART}, {"32", 26, PIN_GPIO},
         {"33", 23, PIN_GPIO|PIN_PWM}, {"GND", -1, PIN_GND},
         {"35", 24, PIN_GPIO}, {"36", 27, PIN_GPIO},
         {"AIN3", 25, PIN_AIN}, {"1.8V", 28, PIN_PWR},
@@ -27,6 +27,7 @@ OdroidM2::OdroidM2() {
     i2cList = {
         {"I2C-1", "/dev/i2c-0", 8, 9}, // Pin #3, #5
         {"I2C-2", "/dev/i2c-1", 30, 31}, // Pin #27, #28
+        {"I2C-3", "/dev/i2c-2", 6, 10}, // Pin #22, #24
     };
 
     pwmList = {
@@ -55,6 +56,9 @@ OdroidM2::OdroidM2() {
     uartList = {
         {"UART-1", "/dev/ttyS0", 16, 15}, // Pin #10, #8
         {"UART-2", "/dev/ttyS1", 0, 2}, // Pin #11, #13
+        {"UART-3", "/dev/ttyS2", 4, 5}, // Pin #16, #18
+        {"UART-4", "/dev/ttyS3", 31, 30}, // Pin #28, #27
+        {"UART-5", "/dev/ttyS4", 21, 22}, // Pin #29, #31
         {"UART-USB-0", "/dev/ttyUSB0", -1, -1},
         {"UART-USB-1", "/dev/ttyUSB1", -1, -1},
         {"UART-ACM-0", "/dev/ttyACM0", -1, -1},
